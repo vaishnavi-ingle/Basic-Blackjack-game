1ab9ba1835b9b5d64ef3c20e11a1407cc015bc4b
vaishnavi-ingle <vaishnavimingle@gmail.com>
2024-08-02 19:36:26 +0530

    ew

---
+++ main.js
@@ -0,0 +1,19 @@
+let firstCard = 7;
+let secondcard = 10;
+let sum = firstCard + secondcard;
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+
+function startGame() {
+  console.log("Hello");
+}
+
+function start() {
+  if (sum <= 20) {
+    console.log("Play another card?");
+  } else if (sum === 21) {
+    console.log("Wooho, blackjack");
+  } else if (sum > 21) {
+    console.log("BUST!");
+  }
+}
