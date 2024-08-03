0dab3b78883640c5eb2976f429ecb3228297884b
vaishnavi-ingle <vaishnavimingle@gmail.com>
2024-08-03 20:02:48 +0530

    new7

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let cards = [firstCard, secondcard];
 let sum = firstCard + secondcard;
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let messageEl = document.getElementById("message-el");
+let isAlive = false;
+let hasBlackJack = false;
 
 function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1;
@@ -17,13 +20,23 @@ function getRandomCard() {
 }
 
 function startGame() {
+  for (i = 1; i < cards.length; i++) {
+    cardsEl.textContent += "Cards: " + cards[i];
+  }
   cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
-    console.log("Play another card?");
+    messageEl.textContent = "Play another card?";
   } else if (sum === 21) {
-    console.log("Wooho, blackjack");
+    messageEl.textContent = "Wooho, blackjack";
   } else if (sum > 21) {
-    console.log("BUST!");
+    messageEl.textContent = "BUST!";
   }
+}
+
+function newCard() {
+  let card = getRandomCard();
+  sum = card + sum;
+  cards.push(card);
+  startGame();
 }
